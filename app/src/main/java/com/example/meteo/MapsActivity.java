053bbeca85384503075808f3536ea21b5d4a1bc5
med <boujdi.medz@gmail.com>
2022-05-27 18:04:48 +0100

    commited

---
+++ app/src/main/java/com/example/meteo/MapsActivity.java
@@ -0,0 +1,69 @@
+package com.example.meteo;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.fragment.app.FragmentActivity;
+
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+    // l'identifiant de l'appel de l'autorisation
+    private double lat;
+    private double lng;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_maps);
+
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Intent intent= getIntent();
+        lat = intent.getDoubleExtra("lat", 0);
+        lng = intent.getDoubleExtra("lon", 0);
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+
+        Log.e("mylog", "----> " + lat+ " "+ lng);
+
+        // créer un objet latLng qui stocke la latitude et la longitude de la localisation actuelle
+        LatLng latLng = new LatLng(lat , lng);
+
+        // Ajouter un Marker de la localisation actuelle dans la carte
+        MarkerOptions markerOptions = new MarkerOptions().position(latLng).title("Je suis là !");
+        googleMap.animateCamera(CameraUpdateFactory.newLatLng(latLng));
+        googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng, 12));
+        googleMap.addMarker(markerOptions);
+
+    }
+
+}
